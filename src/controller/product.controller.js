c408b3df8b5ee71339032441be4e451b00f91687
beesaiyvoud <laoseasybee@gmail.com>
2024-08-22 22:41:17 +0700

    add

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -1,19 +1,20 @@
-import { UploadImageToCloud } from "../config/cloudinary";
-import connected from "../config/db";
-import { EMessage, SMessage } from "../service/message";
+import { UploadImageToCloud } from "../config/cloudinary.js";
+import connected from "../config/db.js";
+import { EMessage, SMessage } from "../service/message.js";
 import {
   SendCreate,
   SendError,
   SendError400,
   SendSuccess,
-} from "../service/response";
-import { ValidateData } from "../service/validate";
-
+} from "../service/response.js";
+import { ValidateData } from "../service/validate.js";
+import {v4 as uuidv4} from "uuid";
 export default class ProductController {
   static async getAll(req, res) {
     try {
-      const category = `Select * from product 
-        INNER JOIN category on categoryID = product.categoryID`;
+      const category = `Select productID,pUuid,name,detail,amount,price,image,category.cUuid,category.title
+       ,product.createdAt,product.updatedAt from product 
+        INNER JOIN category  on category.cUuid = product.categoryID`;
       connected.query(category, (err, result) => {
         if (err)
           return SendError(res, 404, EMessage.NotFound + " product", err);
@@ -27,16 +28,35 @@ export default class ProductController {
   }
   static async getOne(req, res) {
     try {
-      const uuid = req.params.cUuid;
-      const checkUuid = `select * from product 
-      INNER JOIN category on categoryID = product.categoryID   
-      where pUuid=?`;
+      const uuid = req.params.pUuid;
+      const checkUuid = `Select productID,pUuid,name,detail,amount,price,image,category.title
+      ,product.createdAt,product.updatedAt from product 
+       INNER JOIN category on category.cUuid = product.categoryID
+       where product.pUuid=? `;
       connected.query(checkUuid, uuid, (err, result) => {
         if (err)
           return SendError(res, 404, EMessage.NotFound + " product", err);
         if (!result[0])
           return SendError(res, 404, EMessage.NotFound + " product");
         return SendSuccess(res, SMessage.SelectOne, result[0]);
+      });
+    } catch (error) {
+      return SendError(res, 500, EMessage.ServerError, error);
+    }
+  }
+  static async getByCategory(req, res) {
+    try {
+      const categoryID = req.params.categoryID;
+      const checkUuid = `Select productID,pUuid,name,detail,amount,price,image,category.title
+      ,product.createdAt,product.updatedAt from product 
+       INNER JOIN category on category.cUuid = product.categoryID
+       where product.categoryID=? `;
+      connected.query(checkUuid, categoryID, (err, result) => {
+        if (err)
+          return SendError(res, 404, EMessage.NotFound + " product", err);
+        if (!result[0])
+          return SendError(res, 404, EMessage.NotFound + " product");
+        return SendSuccess(res, SMessage.SelectOne, result);
       });
     } catch (error) {
       return SendError(res, 500, EMessage.ServerError, error);
@@ -98,13 +118,13 @@ export default class ProductController {
   static async updateProduct(req, res) {
     try {
       const pUuid = req.params.pUuid;
-      const { name, detail, amount, price, categoryID } = req.body;
+      const { name, detail, amount, price, categoryID ,oldImage} = req.body;
       const validate = await ValidateData({
         name,
         detail,
         amount,
         price,
-        categoryID,
+        categoryID,oldImage
       });
       if (validate.length > 0) {
         return SendError400(res, EMessage.PleaseInput + validate.join(","));
@@ -117,21 +137,22 @@ export default class ProductController {
           return SendError(res, 404, EMessage.NotFound, " category");
         const image = req.files;
         if (!image) return SendError400(res, EMessage.BadRequest + " Image");
-        const image_url = await UploadImageToCloud(image.image.data);
+        const image_url = await UploadImageToCloud(image.image.data,oldImage);
         if (!image_url) return SendError400(res, EMessage.ErrorUploadImage);
 
         const datetime = new Date()
           .toISOString()
           .replace(/T/, " ")
           .replace(/\..+/, "");
-        const update = `update product set name=? , detail=?, amount=? , price=? , 
-          categoryID=? image=? ,updatedAt=? 
+        const update = `update product set name=?,detail=?, amount=? , price=? , 
+          categoryID=? , image=? ,updatedAt=? 
           where pUuid=?`;
         connected.query(
           update,
           [name, detail, amount, price, categoryID, image_url, datetime, pUuid],
           (error) => {
-            if (error) return SendError(res, 404, EMessage.ErrorUpdate, err);
+            if (error) return SendError(res, 404, EMessage.ErrorUpdate, error);
+            console.log(error);
             return SendSuccess(res, SMessage.Update);
           }
         );
--- src/router/route.js
+++ src/router/route.js
@@ -2,6 +2,7 @@ import express from "express";
 import AuthController from "../controller/auth.controller.js";
 import BannerController from "../controller/banner.controller.js";
 import CategoryController from "../controller/category.controller.js";
+import ProductController from "../controller/product.controller.js";
 import { auth } from "../middleware/auth.js";
 const router = express.Router();
 //-------- auth -------
@@ -24,4 +25,12 @@ router.get("/category/getAll", CategoryController.getAll);
 router.get("/category/getOne/:cUuid", CategoryController.getOne);
 router.put("/category/update/:cUuid",auth,CategoryController.updateCategory);
 router.delete("/category/delete/:cUuid",auth,CategoryController.deleteCategory);
+// ------- product -----
+router.post("/product/insert",auth,ProductController.insert);
+router.get("/product/getAll", ProductController.getAll);
+router.get("/product/getOne/:pUuid", ProductController.getOne);
+router.get("/product/getBy/:categoryID", ProductController.getByCategory);
+router.put("/product/update/:pUuid",auth,ProductController.updateProduct);
+router.delete("/product/delete/:pUuid",auth,ProductController.deleteProduct);
+
 export default router;
